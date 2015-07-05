733ca5673768151b02af5f7e24ac0140eb6a3b34
Vincent Demeester <vincent@sbr.pm>
2015-07-05 14:07:40 +0200

    First exec implementation

--- src/praetorian/setup.go
+++ src/praetorian/setup.go
@@ -20,11 +20,11 @@ var SetupCommand = cli.Command{
 
 func setup(c *cli.Context) {
 	if len(c.Args()) != 2 {
-		fmt.Fprintf(os.Stderr, "setup commands needs two arguments : praetorian setup user alias\n")
+		fmt.Fprintf(os.Stderr, "setup commands needs two arguments : praetorian setup user name\n")
 		os.Exit(1)
 	}
 	username := c.Args().Get(0)
-	alias := c.Args().Get(1)
+	name := c.Args().Get(1)
 
 	// Does the user exists
 	user, err := u.Lookup(username)
@@ -76,7 +76,7 @@ func setup(c *cli.Context) {
 
 	// Put the magic string at the end of sshConfFile
 	finalKey := strings.TrimSpace(string(key))
-	sshMagicString := fmt.Sprintf(`%s command="FINGERPRINT=%s NAME=%s praetorian exec $SSH_ORIGINAL_COMMAND",no-X11-forwarding`, finalKey, keyFingerPrint, alias)
+	sshMagicString := fmt.Sprintf(`%s command="FINGERPRINT=%s NAME=%s praetorian exec $SSH_ORIGINAL_COMMAND",no-X11-forwarding`, finalKey, keyFingerPrint, name)
 
 	f, err := os.OpenFile(sshConfFile, os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
