f2a5c76a307614b7701f953b27d34de6ce783a33
Vincent Demeester <vincent@sbr.pm>
2015-07-07 15:54:21 +0200

    Fix invalid package names

--- src/cmd/praetorian/main.go
+++ src/cmd/praetorian/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 	"praetorian"
-	commands "praetorian/commands"
+	"praetorian/commands"
 
 	"github.com/codegangsta/cli"
 )
--- src/praetorian/commands/setup.go
+++ src/praetorian/commands/setup.go
@@ -1,4 +1,4 @@
-package praetorian
+package commands
 
 import (
 	"fmt"
