38f0307a75dcc2b7f84024faf1edc040f2d83886
Vincent Demeester <vincent@sbr.pm>
2015-07-07 15:53:24 +0200

    Extract version to version.go

--- src/cmd/praetorian/main.go
+++ src/cmd/praetorian/main.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"os"
+	"praetorian"
 	commands "praetorian/commands"
 
 	"github.com/codegangsta/cli"
 )
 
-const (
-	// NAME name of the application
-	NAME = "praetorian"
-	// VERSION version of the application
-	VERSION = "0.5.0-dev"
-	// DESCRIPTION description of the application
-	DESCRIPTION = "A ssh praetorian (bouncer, minder or whatever) ; it's just a cool restricted command script."
-)
-
 func main() {
 	app := cli.NewApp()
-	app.Name = NAME
-	app.Version = VERSION
-	app.Usage = DESCRIPTION
+	app.Name = praetorian.Name
+	app.Version = praetorian.Version
+	app.Usage = praetorian.Description
 	app.Commands = []cli.Command{
 		commands.ExecCommand,
 		commands.SetupCommand,
---
+++ src/praetorian/version.go
@@ -0,0 +1,11 @@
+package praetorian
+
+// Name is executable name of this application.
+const Name = "praetorian"
+
+// Version is version string of this application.
+// Version is changed with semantic versioning.
+const Version string = "0.5.0-dev"
+
+// Description is the simple description of this application
+const Description = "A ssh praetorian (bouncer, minder or whatever) ; it's just a cool restricted command script."
